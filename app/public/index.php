be437134f6adda2406acf71fbcc6e355b763c743
662781 <70585970+662781@users.noreply.github.com>
2021-12-16 09:52:39 +0100

    Les 6
    API's

--- app/public/index.php
+++ app/public/index.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <input id="red" type="range" value="0" min=0 max=255 onchange="changeColor()">
-    <input id="green" type="range" value="0" min=0 max=255 onchange="changeColor()">
-    <input id="blue" type="range" value="0" min=0 max=255 onchange="changeColor()">
+<?php
+require __DIR__ . '/../patternrouter.php';
 
-    <script src="les5.js"></script>
-</body>
-</html>
+$uri = trim($_SERVER['REQUEST_URI'], '/');
+
+$router = new PatternRouter();
+$router->route($uri);
---
+++ app/public/les5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <input id="red" type="range" value="0" min=0 max=255 onchange="changeColor()">
+    <input id="green" type="range" value="0" min=0 max=255 onchange="changeColor()">
+    <input id="blue" type="range" value="0" min=0 max=255 onchange="changeColor()">
+
+    <script src="les5.js"></script>
+</body>
+</html>
